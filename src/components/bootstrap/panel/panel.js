4d49e4dcf82d19db567c4b6ec86f79e7cfc15fd2
Facundo Lespi <flespi@bpba.com.ar>
2015-09-14 11:14:03 -0300

    Hago el panel expandible

--- src/components/bootstrap/panel/panel.js
+++ src/components/bootstrap/panel/panel.js
@@ -8,14 +8,26 @@ define(['knockout', 'quark', 'text!./panel.html'], function(ko, $$, templateMark
     this.pie;
 
     this.color;
+    this.expansible;
+    this.expanded;
 
     $$.parameters({
-        color: ko.observable('default')
+        color: ko.observable('default'),
+        expansible: ko.observable(false),
+        expanded: ko.observable(true)
     }, params, this);
 
     this._css_ = ko.pureComputed(function() {
         return 'panel panel-' + self.color();
     }, this);
+
+    this._expand_css_ = ko.pureComputed(function() {
+        return 'glyphicon ' + ( self.expanded() ? 'glyphicon-menu-up' : 'glyphicon-menu-down' );
+    }, this);
+
+    this.expand_toggle = function() {
+      self.expanded(!self.expanded());
+    };
 
   }
 
