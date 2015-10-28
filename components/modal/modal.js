dafbf9fe7387276e2ad50248147c3195b0d97f50
admin <fllopez@bpba.com.ar>
2015-10-28 19:15:43 -0300

    Ajustes sobre modal que no limpiaba el backdrop porque se eliminaba el componente antes de terminar la animacion

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -8,16 +8,27 @@ define(['knockout', 'quark', 'text!./modal.html'], function(ko, $$, template) {
             element = elem;
         }
 
-        this.show = $scope.show = function() {
+        this.show = function() {
             $(element).modal('show');
         }
 
-        this.hide = $scope.hide = function() {
+        this.hide = function() {
             $(element).modal('hide');
         }
 
-        this.toggle = $scope.toggle = function() {
+        this.toggle = function() {
             $(element).modal('toggle')
         }
+
+        this.dispose = function() {
+            // Al cerrar el componente cierra la pagina.
+            self.hide();
+
+            // Como el componente muere antes de sacar el backdrop por la animacion limpio
+            // el backdrop y el propio modal.
+            $('.modal-backdrop').remove();
+            $(element).remove();
+        }
+
     }, template);
 });
