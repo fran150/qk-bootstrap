fcf15bcaec378f7f03cdc6f8875a9a8508555d35
admin <fllopez@bpba.com.ar>
2015-11-02 18:01:45 -0300

    Propiedad size en el modal para cambiarle el tamaño

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -2,7 +2,25 @@ define(['knockout', 'quark', 'text!./modal.html'], function(ko, $$, template) {
     return $$.component(function(params, $scope) {
         var self = this;
 
+        var sizes = {
+            sm: 'modal-sm',
+            md: '',
+            lg: 'modal-lg'
+        }
+
         var element;
+
+        $$.parameters({
+            size: ko.observable('md')
+        }, params, this);
+
+        $scope.sizeStyle = ko.pureComputed(function() {
+            if (sizes[self.size()]) {
+                return sizes[self.size()];
+            } else {
+                return sizes['md'];
+            }
+        });
 
         $scope.getElement = function(elem) {
             element = elem;
