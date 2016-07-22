4591334ed235cc1432958c51567ada2f53268a80
admin <fllopez@bpba.com.ar>
2016-07-22 18:01:06 -0300

    Move the navbar to alchemy

--- main.js
+++ main.js
@@ -8,14 +8,8 @@ define(['module', 'knockout', 'jquery', 'quark'], function(mod, ko, $, $$) {
         components: {
             "alert": "components/alert/alert",
             "modal": "components/modal/modal",
-            "navbar": "components/navbar/navbar",
-            "navbar-alert": "components/navbar/controls/alert",
-            "navbar-button": "components/navbar/controls/button",
-            "navbar-dropdown": "components/navbar/controls/dropdown",
-            "navbar-link": "components/navbar/controls/link",
             "progress": "components/progress/progress",
             "tabs": "components/tabs/tabs"
         }
     });
 });
-
